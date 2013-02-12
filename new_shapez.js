fa57c8b92928fc27b7e309e2da94d2ca5512f0b8
Ryan Kois <kid_icarus@1482934.no-reply.drupal.org>
2013-02-11 22:01:24 -0700

    Newness.

--- new_shapez.js
+++ new_shapez.js
@@ -2,19 +2,36 @@ var setup = function() {
   var x = 0;
   var y = 0;
   var frameRate = 1000/300;
+  var AUTUMN = 4000;
+  var SEA = 1000;
+  var colorscheme = SEA;
   var canvas = document.getElementById('dacanvas');
-  canvas.width = 600;
-  canvas.height = 600;
+  var clicker = 0;
+  canvas.width = document.width;
+  canvas.height = document.height;
 
   var ctx = canvas.getContext('2d');
+  canvas.onclick = function() {
+    clicker = (clicker + 1) % 2;
+    if (colorscheme == AUTUMN) {
+      colorscheme = SEA;
+    }
+    else {
+      colorscheme = AUTUMN;
+    }
+    var snd = new Audio('geese.wav');
+    snd.play();
+  }
   var drawSquare = function() {
+    if(clicker) {
+    }
     x = 0;
     y = 0;
-    var randX = (Math.random() * 600) + 1;
-    var randY = (Math.random() * 600) + 1;
-    var randSize = (Math.random() * 80) + 1;
+    var randX = (Math.random() * canvas.width) + 1;
+    var randY = (Math.random() * canvas.height) + 1;
+    var randSize = (Math.random() * canvas.height / 20) + 1;
     var rand = (Math.random() * 200) + 1;
-    var filly = '#'+Math.floor(Math.random()*16777215).toString(16);
+    var filly = '#'+Math.floor((Math.random()*1000)+colorscheme).toString(16);
     ctx.beginPath();
     ctx.arc(randX, randY, randSize, 0, Math.PI*2, false);  
     ctx.closePath();
